375a19aebfa3342bec62a7378664705405680b88
AbhishekRauthan <abhishek.rauthan.10@gmail.com>
2020-08-22 23:17:49 +0530

    Initial Comment; Netflix clone made with Angular; Added Rows

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RowsComponent } from './rows/rows.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RowsComponent
   ],
   imports: [
     BrowserModule,
