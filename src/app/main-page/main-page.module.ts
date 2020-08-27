1e32a35663700fa2b856c4440fcdd7630efa887a
AbhishekRauthan <abhishek.rauthan.10@gmail.com>
2020-08-27 14:39:20 +0530

    added navbar and completed banner

--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RowsComponent } from './rows/rows.component';
 import { ExtendComponent } from './extend/extend.component';
 import { BannerComponent } from './banner/banner.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
-  declarations: [RowsComponent, ExtendComponent, BannerComponent],
+  declarations: [RowsComponent, ExtendComponent, BannerComponent, NavbarComponent],
   imports: [
     CommonModule
   ],
